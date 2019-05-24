908d1a9ff643c7afe79dd4ade7c0a374a88f7462
Edward Korkollie <korkollieed@gmail.com>
2019-05-24 12:14:13 -0400

    lab

--- Main.java
+++ Main.java
@@ -1,9 +1,26 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){
+        
+        int number;
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Please Enter Any Number: ");
+        number = input.nextInt();
+        int Sum = 0;
+        for (int i = 1; i <= number; i++)
+        {
+            
+            //number = input.nextInt();
+            Sum += i;
+        }
+        System.out.println("The Sum of the number you enter is: " + Sum);
+        
 
     }
 }
